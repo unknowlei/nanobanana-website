30e851c5a4aaca6a5d6af1742cc3abd6b0394df4
不明真相的雷君 <90095804+unknowlei@users.noreply.github.com>
2025-11-28 23:12:39 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import {
  * 👇👇👇 核心配置区 👇👇👇
  * ==============================================================================
  */
-const DATA_SOURCE_URL = "https://raw.githubusercontent.com/unknowlei/nanobanana-data/refs/heads/main/data%20(23).json";
+const DATA_SOURCE_URL = "https://raw.githubusercontent.com/unknowlei/nanobanana-data/refs/heads/main/data%20(29).json";
 
 // 📧 EmailJS 配置
 const EMAILJS_SERVICE_ID = "service_4y3xdta";    
